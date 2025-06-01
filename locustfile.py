ab4844f2409d85ffdd6348f4fc9a254c8d7b414f
Girish dev kumar Chaurasiya <girishdevkumarchaurasiya@Girishs-Laptop.local>
2025-06-01 16:33:25 +0530

    Revert "initial agentic ai commit"
    This reverts commit 09b57f3a751ace080acfeb07a581c693868470b5.

--- locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class AgentUser(HttpUser):
-    wait_time = between(1, 2)
-
-    @task
-    def send_query(self):
-        self.client.get("/run", params={"query": "Tell me something interesting about AI"})
